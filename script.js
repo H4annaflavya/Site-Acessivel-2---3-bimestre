3ee3a6ef5430ebf5fe4c548fa0fc871d5ced1c60
H4annaflavya <168755002+H4annaflavya@users.noreply.github.com>
2025-09-16 20:24:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -39,6 +39,6 @@ document.addEventListener('DOMContentLoaded', function () {
 })
 
  ScrollReveal().reveal('#inicio', { delay: 500 });
- ScrollReveal().reveal('#tropicalia', { delay: 500 });
+ ScrollReveal().reveal('#rihanna', { delay: 500 });
  ScrollReveal().reveal('#galeria', { delay: 500 });
  ScrollReveal().reveal('#contato', { delay: 500 });
